deed769f59b2407b9a223af950c94d7e823daefb
Sonny Achten <sonny.achten@kuleuven.be>
2024-05-21 14:29:31 +0200

    Update definitions.py
    fix out dir

--- definitions.py
+++ definitions.py
@@ -14,7 +14,7 @@ TensorType = torch.FloatTensor  # HalfTensor, FloatTensor, DoubleTensor
 torch.set_default_tensor_type(TensorType)
 
 ROOT_DIR = pathlib.Path(os.path.dirname(os.path.abspath(__file__))) # This is your Project Root
-OUT_DIR = pathlib.Path("~/out/mprkm/").expanduser()
+OUT_DIR = pathlib.Path("~/out/hencler/").expanduser()
 OUT_DIR.mkdir(parents=True, exist_ok=True)
 DATA_DIR = pathlib.Path("~/data").expanduser()
 np_rand = np.random.RandomState(42)
